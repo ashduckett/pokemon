a2326488adfe2f60dfc425a5c87e78f74c13110e
Ash <ash.duckett@outlook.com>
2022-06-15 14:16:40 +0100

    Clear up project and add UISearchBar

---
+++ Pokemon/PokemonListVC.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Ash Duckett on 15/06/2022.
+//
+
+import UIKit
+
+class PokemonListVC: UIViewController {
+    var safeArea: UILayoutGuide!
+    let searchBar = UISearchBar()
+    
+    override func viewDidLoad() {
+        // This doesn't appear to work. Why not?
+        view.backgroundColor = .white
+        safeArea = view.layoutMarginsGuide
+        
+        
+        view.addSubview(searchBar)
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        view.backgroundColor = .white
+        
+    }
+
+
+}
+
